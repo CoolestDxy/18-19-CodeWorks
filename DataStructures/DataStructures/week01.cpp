d41f99f340b5a483eb8582376ee3cb3d6ffdf9c9
Hanyuu <HanyuuFurude@outlook.com>
2018-09-22 19:12:59 +0800

    201809221912_finish_the_work

--- DataStructures/DataStructures/week01.cpp
+++ DataStructures/DataStructures/week01.cpp
@@ -2,16 +2,25 @@
 //Page:032 No:02
 #include <iostream>
 #include <string>
+#include <list>
 
 unsigned userInput{ 0 };
 bool* list = new bool[userInput] {false};
+std::list<std::string> sets = { "a","b","c","d","e","f" };
 
 void P032N02_R(const unsigned);
+void P032N14_R(const unsigned);
 void P032N02_Print();
+void P032N14_Print();
 void P032N02()
 {
+	std::cout << "Please input the size of the list(for Page032 Question02):";
 	std::cin >> userInput;
 	P032N02_R(userInput);
+	std::cout << "{ ";
+	P032N14_R(sets.size());
+	std::cout << " }";
+
 }
 void P032N02_R(const unsigned n)
 {
@@ -52,4 +61,37 @@ void P032N02_Print()
 			std::cout << " ;\n";
 		}
 	}
+}
+
+void P032N14_R(const unsigned n)
+{
+	if (n == 1)
+	{
+		list[n - 1] = true;
+		P032N14_Print();
+		list[n - 1] = false;
+		P032N14_Print();
+	}
+	else
+	{
+		list[n - 1] = true;
+		P032N14_R(n - 1);
+		list[n - 1] = false;
+		P032N14_R(n - 1);
+	}
+}
+void P032N14_Print()
+{
+	std::cout << "( ";
+	int n = 0;
+	for (std::list<std::string>::iterator i = sets.begin(); i != sets.end(); i++)
+	{
+		if (list[n])
+		{
+			std::cout << *i;
+
+		}
+		++n;
+	}
+	std::cout << " )";
 }
