80828b5c8a3eba4c0a21705347b6813613dc5de1
Hanyuu <HanyuuFurude@outlook.com>
2018-09-22 17:00:38 +0800

    201809221700

---
+++ DataStructures/DataStructures/main.cpp
@@ -0,0 +1,10 @@
+//main.cpp
+//工程运行入口
+#define LAUNCH P032N02
+
+void LAUNCH();
+int main(int argc,char * argv[])
+{
+	LAUNCH();
+	return 0;
+}
---
+++ DataStructures/DataStructures/week01.cpp
@@ -0,0 +1,55 @@
+//week01.cpp
+//Page:032 No:02
+#include <iostream>
+#include <string>
+
+unsigned userInput{ 0 };
+bool* list = new bool[userInput] {false};
+
+void P032N02_R(const unsigned);
+void P032N02_Print();
+void P032N02()
+{
+	std::cin >> userInput;
+	P032N02_R(userInput);
+}
+void P032N02_R(const unsigned n)
+{
+	if (n==1)
+	{
+		list[n-1] = true;
+		P032N02_Print();
+		list[n-1] = false;
+		P032N02_Print();
+	}
+	else
+	{
+		list[n-1] = true;
+		P032N02_R(n - 1);
+		list[n-1] = false;
+		P032N02_R(n - 1)
+	}
+}
+
+void P032N02_Print()
+{
+	for (int i = 0; i < userInput; i++)
+	{
+		if (list[i])
+		{
+			std::cout << "true";
+		}
+		else
+		{
+			std::cout << "false";
+		}
+		if (i!=userInput-1)
+		{
+			std::cout << ", ";
+		}
+		else
+		{
+			std::cout << " ;\n";
+		}
+	}
+}
