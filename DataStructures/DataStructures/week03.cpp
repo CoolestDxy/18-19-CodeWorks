efa3156e48b2cf56654f4879ef311b8875369a74
Hanyuu <HanyuuFurude@outlook.com>
2018-10-11 23:21:30 +0800

    201810112321

--- DataStructures/DataStructures/week03.cpp
+++ DataStructures/DataStructures/week03.cpp
@@ -5,5 +5,25 @@ How much time does it take to locate an arbitrary element A[i][j] in the represe
 时间复杂度O(m*n);
 P107N02
 Analyze carefully the computing tiem and storge requirements of function FastTranspace. What can you say about the existence of an even faster algotithm.
-
+时间复杂度
+time=terms+terms
+O(n)
+空间复杂度
+useage=col+col+term
+O(n)
 */
+//P107N04
+//Rewrite function FastTranspose……
+class SparseMatrix
+{
+	SparseMatrix(int r, int c, int t);
+	SparseMatrix FastTranspose();
+};
+SparseMatrix::SparseMatrix(int r, int c, int t)
+{
+
+}
+SparseMatrix SparseMatrix::FastTranspose()
+{
+
+}
