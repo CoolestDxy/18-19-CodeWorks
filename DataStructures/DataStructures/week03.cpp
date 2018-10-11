dd0a16de467ca10f939ddd4cf1c2cc827a6a1edc
Hanyuu <HanyuuFurude@outlook.com>
2018-10-11 22:43:25 +0800

    201810112243

---
+++ DataStructures/DataStructures/week03.cpp
@@ -0,0 +1,9 @@
+//week03.cpp
+/*
+P107N01
+How much time does it take to locate an arbitrary element A[i][j] in the representation of this section and to change its value;
+ ±º‰∏¥‘”∂»O(m*n);
+P107N02
+Analyze carefully the computing tiem and storge requirements of function FastTranspace. What can you say about the existence of an even faster algotithm.
+
+*/
