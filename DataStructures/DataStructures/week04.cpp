969403f6e89516704611a520c641cae390482433
Hanyuu <HanyuuFurude@outlook.com>
2018-10-14 20:13:33 +0800

    201810142013

--- DataStructures/DataStructures/week04.cpp
+++ DataStructures/DataStructures/week04.cpp
@@ -10,18 +10,21 @@
 //#define MAX_N 40
 //#define MIN_M 2
 //#define MAX_M 20
-
-#define M 15
-#define N 15
+//
+//#define M 20
+//#define N 15
+#define M 10
+#define N 10
 #define INIT_M 9
 #define INIT_N 9
+#define MAX_STEP 50000
 
 struct Bug
 {
 	int i;
 	int j;
 };
-inline int * avliableList(Bug me,int * res)
+inline void avliableList(Bug me,int * res)
 {
 	switch (me.i)
 	{
@@ -32,19 +35,19 @@ inline int * avliableList(Bug me,int * res)
 			//int * res = new int[4];
 			res[0] = 3;	//Total
 			res[1] = 1;res[2] = 2;res[3] = 3;
-			return res;
+			return;
 			break;
 		case N:
 			//int * res = new int[4];
 			res[0] = 3;	//Total
 			res[1] = 3; res[2] = 4; res[3] = 5;
-			return res;
+			return;
 			break;
 		default:
 			//int * res = new int[6];
 			res[0] = 5;	//Total
 			res[1] = 1; res[2] = 2; res[3] = 3; res[4] = 4; res[5] = 5;
-			return res;
+			return;
 			break;
 		}
 		break;
@@ -55,19 +58,19 @@ inline int * avliableList(Bug me,int * res)
 			//int * res = new int[4];
 			res[0] = 3;	//Total
 			res[1] = 0; res[2] = 1; res[3] = 7;
-			return res;
+			return;
 			break;
 		case N:
 			//int * res = new int[4];
 			res[0] = 3;	//Total
 			res[1] = 5; res[2] = 6; res[3] = 7;
-			return res;
+			return;
 			break;
 		default:
 			//int * res = new int[6];
 			res[0] = 5;	//Total
 			res[1] = 0; res[2] = 1; res[3] = 5; res[4] = 6; res[5] = 7;
-			return res;
+			return;
 			break;
 		}
 		break;
@@ -78,19 +81,19 @@ inline int * avliableList(Bug me,int * res)
 			//int * res = new int[6];
 			res[0] = 5;	//Total
 			res[1] = 0; res[2] = 1; res[3] = 2; res[4] = 3; res[5] = 7;
-			return res;
+			return;
 			break;
 		case N:
 			//int * res = new int[6];
 			res[0] = 5;	//Total
 			res[1] = 3; res[2] = 4; res[3] = 5; res[4] = 6; res[5] = 7;
-			return res;
+			return;
 			break;
 		default:
 			//int * res = new int[9];
 			res[0] = 8;	//Total
-			res[1] = 1; res[2] = 2; res[3] = 3; res[4] = 4; res[5] = 5; res[6] = 6; res[7] = 7;
-			return res;
+			res[1] = 0; res[2] = 1; res[3] = 2; res[4] = 3; res[5] = 4; res[6] = 5; res[7] = 6; res[8] = 7;
+			return;
 			break;
 		}
 		break;
@@ -101,7 +104,8 @@ void P123N08()
 	srand(time(0));
 	int matrix[M][N] = { 0 };
 	int overRide = 1;
-	int * res;
+	int step = 0;
+	int * res = new int[9];
 	int * resList;
 	int stepCode;
 	const int imove[8] = { -1,0,1,1,1,0,-1,-1 };
@@ -109,12 +113,29 @@ void P123N08()
 	Bug bug{ INIT_M,INIT_N };
 	++matrix[INIT_M][INIT_N];
 	std::cout << "Searching started" << std::endl;
-	while (overRide < M*N)
+	while (overRide < M*N && step<MAX_STEP)
 	{
-		resList = avliableList(bug,res);
-		stepCode = resList[rand() % resList[0] + 1];
+		avliableList(bug, res);
+		stepCode = res[rand() % res[0] + 1];
 		bug.i += imove[stepCode]; bug.j += jmove[stepCode];
 		++matrix[bug.i][bug.j];
 		matrix[bug.i][bug.j] == 1 ? ++overRide:NULL;
+		++step;
 	}
+	if (step>=MAX_STEP)
+	{
+		std::cout << "Step overRide." << std::endl;
+	}
+	else
+	{
+		for (int i = 0; i < M; i++)
+		{
+			for (int j = 0; j < N; j++)
+			{
+				std::cout << matrix[i][j]<<"\t";
+			}
+			std::cout << std::endl;
+		}
+	}
+	//exit(0);
 }
