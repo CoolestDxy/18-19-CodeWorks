f8bf4d655b1a8438d14f7751ee7f1d767ebe8484
Hanyuu <HanyuuFurude@outlook.com>
2018-10-20 23:27:37 +0800

    201810202327

--- DataStructures/DataStructures/week05.cpp
+++ DataStructures/DataStructures/week05.cpp
@@ -1,6 +1,7 @@
 //week05.cpp
 //P138N01
 //Split a stack into two and merge them.
+#include<iostream>
 template <class T>
 class Stack
 {
@@ -13,7 +14,7 @@ public:
 	T& Ttop()const { if (isEmpty()) throw "Stack is empty:"; return stack[pop]; }
 	void push(const T& item)
 	{
-		if (pop==capacity-1)
+		if (top == capacity - 1)
 		{
 			T * temp = new T[2 * capacity];
 			for (int i = 0; i < capacity; i++)
@@ -23,12 +24,68 @@ public:
 			delete[] stack;
 			stack = temp;
 			capacity *= 2;
-			stack[++pop] = item;
 		}
+		stack[++top] = item;
 	}
 	void pop() { if (isEmpty())throw "stack is empty"; stack[top--].~T(); }
+	Stack* split()
+	{
+		Stack * tempStk = new Stack(top - top / 2);
+		//T* temp = new T[top-top / 2];
+		for (int i = top / 2 + 1; i <= top; i++)
+		{
+			tempStk->stack[i - top / 2 - 1] = stack[i];
+			stack[i].~T();
+			++tempStk->top;
+		}
+		top -= (1 + tempStk->top);
+		return tempStk;
+	}
+	void merge(Stack<T>* stk)
+	{
+		int newCap = capacity + stk->capacity;
+		if (newCap > capacity)
+		{
+			T * temp = new T[newCap];
+			for (int i = 0; i < capacity; i++)
+			{
+				temp[i] = stack[i];
+			}
+			delete[] stack;
+			stack = temp;
+			capacity = newCap;
+		}
+		for (int i = top; i <= capacity; i++)
+		{
+			stack[i + 1] = stk->stack[i - top];
+		}
+		top += stk->top + 1;
+	}
+	void print()
+	{
+		for (int i = 0; i <= top; i++)
+		{
+			std::cout << stack[i] << " ";
+		}
+		std::cout << std::endl;
+	}
 };
 void P138N01()
 {
-
+	Stack<int> exp(30);
+	for (int i = 0; i < 30; i++)
+	{
+		exp.push(i);
+	}
+	exp.print();
+	Stack<int> * expS = exp.split();
+	exp.print();
+	expS->print();
+	exp.merge(expS);
+	exp.print();
 }
+//P138N02
+/*
+倒车调度问题：
+	
+*/
