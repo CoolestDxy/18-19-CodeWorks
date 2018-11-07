0c70c7ff57c3a29d0e2195d3932579727c970261
Hanyuu Furude <HanyuuFurude@outlook.com>
2018-11-07 10:30:04 +0800

    201811071030

---
+++ Java/Java_Eclipse/Java_Eclipse/src/src/HelloWorld.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package src;
+import java.util.Scanner;
+
+/**
+ * @author HanyuuFurude
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+		Scanner a=new Scanner(System.in);
+		String n=a.next();
+		System.out.println(n);
+		a.close();
+	}
+
+}
