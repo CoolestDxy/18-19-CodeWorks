5f71c31d45887b9a18616487b645abb72dddf474
Hanyuu Furude <HanyuuFurude@outlook.com>
2018-11-11 00:24:38 +0800

    201811110024

--- DataStructures/DataStructures/week08.cpp
+++ DataStructures/DataStructures/week08.cpp
@@ -1,60 +1,118 @@
 //week08.cpp
-#include"public.h"
+#include "public.h"
 #include <stack>
 template <typename T>
 struct Node
 {
-	Node<T> * left;
-	Node<T> * right;
+	Node<T> *left;
+	Node<T> *right;
 	T data;
 };
 template <typename T>
 class Tree
 {
-public:
+  public:
 	Tree() { root = new Node<T>; }
-	~Tree() {/*TODO删除树*/ };
-	void delTree(Node<T> *node) 
+	~Tree(){/*TODO删除树*/};
+	void delTree(Node<T> *node)
 	{
-		if (node->left) { delTree(node->left); }
-		if (node->right) { elTree(node->right); }
+		if (node->left)
+		{
+			delTree(node->left);
+		}
+		if (node->right)
+		{
+			elTree(node->right);
+		}
 		delete[] node;
 	};
 	void inorder() { inorder(root); };
-	void inorder(Node<T> * node) { if (node) { inorder(node->left); std::cout << node->data; inorder(node->right); } }
+	void inorder(Node<T> *node)
+	{
+		if (node)
+		{
+			inorder(node->left);
+			std::cout << node->data;
+			inorder(node->right);
+		}
+	}
 	void preorder() { inorder(root); };
-	void preorder(Node<T> * node) { if (node) { std::cout << node->data; preorder(node->left);  preorder(node->right); } }
+	void preorder(Node<T> *node)
+	{
+		if (node)
+		{
+			std::cout << node->data;
+			preorder(node->left);
+			preorder(node->right);
+		}
+	}
 	void postorder() { inorder(root); };
-	void postorder(Node<T> * node) { if (node) { std::cout << node->data; postorder(node->left);  postorder(node->right); } }
+	void postorder(Node<T> *node)
+	{
+		if (node)
+		{
+			std::cout << node->data;
+			postorder(node->left);
+			postorder(node->right);
+		}
+	}
 
-private:
+  private:
 	Node<T> root;
 };
 template <typename T>
-class inorderIterator 
+class inorderIterator
 {
-public:
-	inorderIterator(Node<T> * node) { currentNode = node; };
-	T* next()
+  public:
+	inorderIterator(Node<T> *node) { currentNode = node; };
+	T *next()
 	{
 		while (currentNode)
 		{
 			s.push(currentNode);
 			currentNode = currentNode->left;
 		}
-		if (s.empty())return nullptr;
-		{
-			currentNode = s.top();
-			s.pop();
-			T& temp = currentNode->data;
-			currentNode = currentNode->right;
-			return &temp;
-		}
+		if (s.empty())
+			return nullptr;
+		currentNode = s.top();
+		s.pop();
+		T &temp = currentNode->data;
+		currentNode = currentNode->right;
+		return &temp; 	
 	}
-private:
-	std::stack<Node<T> * > s;
-	Node<T> * currentNode;
+
+  private:
+	std::stack<Node<T> *> s;
+	Node<T> *currentNode;
+};
+template <typename T>
+class forwardIterator
+{
+  public:
+	forwardIterator(Node<T> *node) { currentNode = node; };
+	T *next()
+	{
+		T *temp = currentNode.data;
+		if(currentNode==nullptr)
+		{
+			currentNode=s.top()->right;
+			s.pop();
+		}
+		else (currentNode->left)
+		{
+			s.push(currentNode);
+			currentNode = currentNode->left;
+		}
+		if(s.empty())
+		{
+			return nullptr;
+		}
+		return &T;
+	}
+
+  private:
+	std::stack<Node<T> *> s;
+	Node<T> *currentNode;
 };
 
 Tree<int>;
-
