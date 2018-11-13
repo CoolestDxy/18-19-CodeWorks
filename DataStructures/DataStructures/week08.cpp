7c4b09badd488a49ba54089c7c03ece4ffe4ada0
Hanyuu Furude <HanyuuFurude@outlook.com>
2018-11-13 21:21:19 +0800

    201811132121

--- DataStructures/DataStructures/week08.cpp
+++ DataStructures/DataStructures/week08.cpp
@@ -7,29 +7,42 @@
 //P267N04 树中序遍历的迭代器 查看class inorderIterator
 //P267N06 无递归的前序遍历器 查看class Tree函数	void preorder_noneRecursive()
 //P273N01 计算树中的叶节点并给出时间复杂度	查看class Tree函数 countLeaf() 时间复杂度O(n)
-//P273N04 删除二叉树中所有节点并给出时间复杂度 查看class Tree函数 	void delTree(Node<T> *node) 时间复杂度O(n)
+//			时间复杂度O(n)
+//P273N04 删除二叉树中所有节点并给出时间复杂度 查看class Tree函数 	void delTree(Node<T> *node)
+//			时间复杂度O(n)
 //P273N10 [实验题]构造拷贝构造四种遍历器；
 //week08_exp.cpp
 #include"public.h"
 #include<stack>
 #include<queue>
+
 template <typename T>
 struct Node
 {
+	Node(T data)
+	{
+		this->data = data;
+		left = nullptr; right = nullptr;
+	};
+	T data;
 	Node<T> *left = nullptr;
 	Node<T> *right = nullptr;
-	T data;
 };
+template <typename T>
+class inorderIterator;
 template <typename T>
 class Tree
 {
+	friend class inorderIterator<T>;
+	friend void displaGenerateTree(Tree<int> * tree);
 public:
-	Tree() { root = new Node<T>; }
+	Tree() { root = nullptr; }
 	Tree(Tree * const target) {
 		root = new Node<T>();
 		root.data = target->root;
 		copySubTree(this.root, target.root);
 	}
+
 	void copySubTree(Node<T>* to, const Node<T>* from)
 	{
 		if (from->left)
@@ -46,6 +59,11 @@ public:
 		}
 	}
 	~Tree() {/*TODO删除树*/ };
+	void delTree()
+	{
+		delTree(root);
+		root = nullptr;
+	}
 	void delTree(Node<T> *node)
 	{
 		if (node->left)
@@ -54,7 +72,7 @@ public:
 		}
 		if (node->right)
 		{
-			elTree(node->right);
+			delTree(node->right);
 		}
 		delete[] node;
 	};
@@ -64,16 +82,16 @@ public:
 		if (node)
 		{
 			inorder(node->left);
-			std::cout << node->data;
+			std::cout << node->data << "\t";
 			inorder(node->right);
 		}
 	}
-	void preorder() { inorder(root); };
+	void preorder() { preorder(root); };
 	void preorder(Node<T> *node)
 	{
 		if (node)
 		{
-			std::cout << node->data;
+			std::cout << node->data << "\t";
 			preorder(node->left);
 			preorder(node->right);
 		}
@@ -84,7 +102,7 @@ public:
 		std::stack<Node<T> *> s;
 		do
 		{
-			std::cout << currentNode.data;
+			std::cout << currentNode.data << "\t";
 			if (currentNode == nullptr)
 			{
 				currentNode = s.top()->right;
@@ -97,27 +115,49 @@ public:
 			}
 		} while (!s.empty());
 	}
-	void postorder() { inorder(root); };
+	void postorder() { postorder(root); };
 	void postorder(Node<T> *node)
 	{
 		if (node)
 		{
-			std::cout << node->data;
 			postorder(node->left);
 			postorder(node->right);
+			std::cout << node->data << "\t";
+
 		}
 	}
 	long countLeaf()
 	{
+		if (root == nullptr)
+		{
+			return 0;
+		}
 		return countLeaf(root);
 	}
 	long countLeaf(Node<T> * node)
 	{
-		if (node == nullptr) return 1;
-		return countLeaf(node->left) + countLeaf(node->right);
+		long l = 0; long r = 0;
+		//if (node == nullptr) return 1;
+		//return countLeaf(node->left) + countLeaf(node->right);
+		if (node->left == nullptr && node->right == nullptr)
+		{
+			return 1;
+		}
+		else
+		{
+			if (node->left != nullptr)
+			{
+				l = countLeaf(node->left);
+			}
+			if (node->right != nullptr)
+			{
+				r = countLeaf(node->right);
+			}
+			return l + r;
+		}
 	}
 private:
-	Node<T> root;
+	Node<T> * root;
 };
 template <typename T>
 class inorderIterator
@@ -251,9 +291,39 @@ class leavelIterator
 private:
 	std::queue<Node<T>*> q;
 };
+void displaGenerateTree(Tree<int> * tree)
+{
+	tree->root = new Node<int>(1);
+	tree->root->left = new Node<int>(2);
+	tree->root->right = new Node<int>(3);
+	tree->root->left->left = new Node<int>(4);
+	tree->root->left->right = new Node<int>(5);
 
+}
 void P267N04()
 {
+	std::cout << "=====Hanyuu=====" << std::endl;
+	Tree<int> * tree = new Tree<int>();
+	displaGenerateTree(tree);
+	std::cout << tree->countLeaf() << std::endl;
+	tree->inorder();
+	std::cout << std::endl;
+	tree->preorder();
+	std::cout << std::endl;
+	tree->postorder();
+	std::cout << std::endl;
+	inorderIterator<int> * intIter = new inorderIterator<int>(tree);
+	int * ptr;
+	do
+	{
+		std::cout<<*(ptr = intIter->next())<<"\t";
+	} while (ptr != nullptr);
+	tree->delTree();
+	std::cout << tree->countLeaf();
+	std::cout << std::endl;
+
+	std::cout << "=====Hanyuu=====" << std::endl;
+
 	//TODO..
 	//添加演示代码
 }
@@ -270,6 +340,8 @@ struct ThreadNode
 };
 template<typename T>
 class ThreadNodeTreeIterator;
+void displaGenerateTree(Tree<int> * tree);
+
 template<typename T>
 class ThreadNodeTree
 {
