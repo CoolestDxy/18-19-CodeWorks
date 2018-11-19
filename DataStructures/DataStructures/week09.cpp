ffdc8a6fb9f922059f156285bf973a741587b0a1
Hanyuu <HanyuuFurude@outlook.com>
2018-11-20 00:44:47 +0800

    201811200044

--- DataStructures/DataStructures/week09.cpp
+++ DataStructures/DataStructures/week09.cpp
@@ -1,6 +1,8 @@
 //P287N02 用C++抽象类定义一个最小堆并给出实现
 //P287N03 插入
 #include <utility>
+#include <math.h>
+#include <ctime>
 template <typename T>
 class MinPQ
 {
@@ -8,16 +10,16 @@ public:
 	virtual ~MinPQ();
 	virtual bool isEmpty() const = 0;
 	virtual const T &top() const = 0;
-	virtual push(const T &) = 0;
+	virtual void push(const T &) = 0;
 	virtual void pop() = 0;
 };
 template <typename T>
-class MinHeap :public MinPQ
+class MinHeap :public MinPQ<T>
 {
 public:
 	MinHeap(int capacity = 10)
 	{
-		if (capacity < 1) { throw "capacity can't be negative or zero." }
+		if (capacity < 1) { throw "capacity can't be negative or zero."; }
 		this->capacity = capacity;
 		heapsize = 0;
 		heap = new T[capacity + 1];
@@ -34,7 +36,7 @@ public:
 	{
 		if (heapsize == capacity)
 		{
-			T temp = new[capacity * 2 + 1];
+			T temp = new T[capacity * 2 + 1];
 			for (int i = 1; i < capacity + 1; i++)
 			{
 				temp[i] = heap[i];
@@ -53,7 +55,7 @@ public:
 	}
 	void pop()
 	{
-		if (capacity < 1) { throw "capacity can't be negative or zero." }
+		if (capacity < 1) { throw "capacity can't be negative or zero."; }
 		heap[1].~T();
 		T lastE = heap[heapsize--];
 
@@ -87,7 +89,7 @@ class Dictionary
 public:
 	virtual bool isEmpty()const = 0;
 	virtual std::pair<K, E> * get(const K&)const = 0;
-	virtual void insert(cosnt std::pair<K, E>&) = 0;
+	virtual void insert(const std::pair<K, E>&) = 0;
 	virtual void del(const K&) = 0;
 private:
 };
@@ -111,7 +113,7 @@ public:
 	{
 		return get(root, k);
 	}
-	std::pair<K, E>* get(TreeNode *p, const K&k)
+	std::pair<K, E>* get(TreeNode<K,E> *p, const K&k)
 	{
 		if (!p)
 		{
@@ -165,7 +167,81 @@ public:
 			root = p;
 		}
 	}
+private:
+	TreeNode<K, E> * root;
 };
-//P298N02
-//P304N01
+//P296N02 随机插入测试
+void ins(BST<int,int> *  t,long n)
+{
+	for (int i = 0; i < n; i++)
+	{
+
+		t->insert({ rand(),rand() });
+	}
+}
+void P296N02()
+{
+	BST<int, int> * exp = new BST<int, int>();
+	srand(time(0));
+	ins(exp, 200);
+}
+//P304N01 用C++模板定义森林
+template<typename T>
+class Forest
+{
+public:
+	Forest * borther;
+	Forest * son;
+	T data;
+};
+template<typename T>
+class Tree
+{
+public:
+	T data;
+	Tree * left;
+	Tree * right;
+};
 //P304N04
+template<typename T>
+void add(Forest<T> *fo,Tree<T> *t0)
+{
+	if (t0->left)
+	{
+		fo->son = t0->left;
+		add(fo->son, t0->left);
+	}
+	if (t0->right)
+	{
+		fo->son->borther = t0->right;
+		add(fo->son->borther, t0->right);
+	}
+}
+template<typename T>
+Forest<T> * tran(Tree<T> *t0, Tree<T> *t1)
+{
+	Forest<T> *  fo;
+	Tree<T> * tt0 = t0;
+	Tree<T> * tt1 = t1;
+	if (t0)
+	{
+		add(fo, t0);
+		if (t1)
+		{
+			fo->borther = t1;
+			add(fo->borther, t1);
+
+		}
+	}
+	else
+	{
+		if (t1)
+		{
+			add(fo, t1);
+		}
+		else
+		{
+			return nullptr;
+		}
+	}
+}
